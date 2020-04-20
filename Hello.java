6c79c4307501a1b29717f70df10089c6b2ae4ca1
NC0116 <63992297+NC0116@users.noreply.github.com>
2020-04-20 13:17:30 +0800

    创建了一个Java文件

---
+++ Hello.java
@@ -0,0 +1,23 @@
+package com.neusoft;
+
+public class Demo03 {
+    public static void main(String[] args) {
+        //标识符：给类、接口、方法、变量等起名字的规范
+        /// JAVA中，标识符由字母、数字、下划线、美元符号组成，
+        //python和c是字母、数字、下划线组成
+        //标识符不能以数字开头
+        //不能和JAVA关键字（保留字）重名
+        //最好做到 见到名字知道它的含义，让别人看得懂 name=“张三”；
+        //1. 类  ，大驼峰的命名方式（每个单词首字母大写，其他全部小写）
+        // 比如NariableDemo01, HelloWorld
+        //2. 变量 ，小驼峰 （从第二个单词开始，首字母大写，其余小写）
+        //例如，studentName ,userName
+        //3. 常量， 所有字母全都大写，单词之间用下划线
+        //例如，MAX_MONEY
+        //4. 包的命名规范，所有的字母都小写，多级包之间使用"."隔开 ，一般都是你公司域名的反写
+        //例如， www.neusoft.com--> com.neusoft.yiliao
+        //5.  方法命名小驼峰
+        //public static void getAge(){}
+
+    }
+}
