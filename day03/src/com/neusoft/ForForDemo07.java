2cef5a3b8096d57ed5f325195a6b29d83fd5d52e
NC0116 <63992297+NC0116@users.noreply.github.com>
2020-04-24 14:46:41 +0800

    Create ForForDemo07.java
    用"*"打印正反三角形

---
+++ day03/src/com/neusoft/ForForDemo07.java
@@ -0,0 +1,41 @@
+package com.neusoft;
+
+//public class ForForDemo07 {
+
+    //用"*"打印正三角形:
+//            *
+//            **
+//            ***
+//            ****
+//            *****
+//    public static void main(String[] args) {
+//        for (int i = 1; i <=5; i++) {
+//            for (int j = 1; j <=i ; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+
+
+    //倒三角形:
+//            *****
+//            ****
+//            ***
+//            **
+//            *
+
+//    public static void main(String[] args) {
+//        for (int i = 1; i <=5 ; i++) {
+//            for (int j = i; j <=5 ; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+
+
+
+
+
