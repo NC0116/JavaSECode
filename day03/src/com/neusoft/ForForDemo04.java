5e77fb72661520abb7ccb809245a73f377be8edc
NC0116 <63992297+NC0116@users.noreply.github.com>
2020-04-24 14:45:12 +0800

    Create ForForDemo04.java
    打印 1 天内 所有时间

---
+++ day03/src/com/neusoft/ForForDemo04.java
@@ -0,0 +1,17 @@
+package com.neusoft;
+
+//打印 1 天内 所有时间格式
+//格式是：12 时 48 分
+
+import javax.swing.*;
+
+//public class ForForDemo04 {
+//    public static void main(String[] args) {
+//        for (int i = 0; i <=23 ; i++) {
+//            for (int j = 0; j <=59 ; j++) {
+//                System.out.print(i+"时"+j+"分\t\n");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
