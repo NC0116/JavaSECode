ca111d7333b8ebca4388cd5ed4125bfc163317fe
NC0116 <63992297+NC0116@users.noreply.github.com>
2020-05-05 09:02:17 +0800

    Create ArrayDemo01.java
    数组

---
+++ day04/src/com/neusoft/ArrayDemo01.java
@@ -0,0 +1,14 @@
+package com.neusoft;
+
+public class ArrayDemo01{
+    public static void main(String[] args) {
+        int[] arr = new int[4];
+        int[] arr2 = new int[3];
+        System.out.println(arr);//[I@4554617c
+        System.out.println(arr2);//[I@74a14482
+
+        //A 0, 0, 0, 0
+        //B 输出的是数组的地址值
+    }
+
+}
