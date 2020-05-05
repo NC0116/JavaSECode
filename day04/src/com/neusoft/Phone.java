ae7f77e6a822d6e593a4bc0b6ba5a3ae4f1f0ffb
NC0116 <63992297+NC0116@users.noreply.github.com>
2020-05-05 09:08:39 +0800

    Create Phone.java
    面向对象1

---
+++ day04/src/com/neusoft/Phone.java
@@ -0,0 +1,27 @@
+package com.neusoft;
+
+public class Phone {
+
+        //品牌
+        String brand;
+        //价格
+        int price;
+        //颜色
+        String color;//成员变量
+
+
+
+
+
+    //方法
+   //给name 打电话
+    public void call(String name) {
+        int a = 45; //局部变量
+        System.out.println("给"+name+"打电话");
+    }
+
+    //发短信
+    public  void sendMessage() {
+        System.out.println("发短信");
+    }
+}
