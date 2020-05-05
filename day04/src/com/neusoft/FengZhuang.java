93e760586b04932819a5b575e026b50ba3395f02
NC0116 <63992297+NC0116@users.noreply.github.com>
2020-05-05 09:09:15 +0800

    Create FengZhuang.java
    面向对象1

---
+++ day04/src/com/neusoft/FengZhuang.java
@@ -0,0 +1,9 @@
+package com.neusoft;
+
+public class FengZhuang {
+    /**
+     * 封装实际上将属性封装起来，属性变为私有的，要提供公有的方法对其访问
+     * private 代表最小权限，可以修饰成员和成员方法，
+     * 被private 修饰的成员变量和成员方法，只有在本类才能访问
+     */
+}
