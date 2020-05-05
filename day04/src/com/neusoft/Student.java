7c534cd439d4da563d3cd7565dc8a7ac78f231a5
NC0116 <63992297+NC0116@users.noreply.github.com>
2020-05-05 09:08:52 +0800

    Create Student.java
    面向对象1

---
+++ day04/src/com/neusoft/Student.java
@@ -0,0 +1,19 @@
+package com.neusoft;
+
+public class Student {
+    public static void main(String[] args) {
+
+    }
+    //成员变量
+    String name;//姓名
+    int age;//年龄
+    //成员方法
+    //爱学习的方法
+
+    public void study() {
+        System.out.println("Good Study!");
+    }
+    public void eat(){
+        System.out.println("学习饿了就要吃饭");
+    }
+}
