306cd2ffdbc4803cf93121908f83671884dac74c
NC0116 <63992297+NC0116@users.noreply.github.com>
2020-05-19 14:24:41 +0800

    Random类

---
+++ day05/src/com/neusoft/Test01Random.java
@@ -0,0 +1,13 @@
+package com.neusoft;
+import java.util.Random;
+public class Test01Random {
+    public static void main(String[] args) {
+        // Random 类用于生成伪随机数
+        Random random = new Random();
+        // [0, n) 的随机整数
+        for (int j = 0; j <10 ; j++) {
+            int i = random.nextInt(1);
+            System.out.println(i);
+        }
+    }
+}
