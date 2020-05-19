caec0364fe10081cd390b0a6045053f9f3c796b2
NC0116 <63992297+NC0116@users.noreply.github.com>
2020-05-19 13:34:14 +0800

    JavaBean

---
+++ day05/src/com/neusoft/StudentJavaBean.java
@@ -0,0 +1,32 @@
+package com.neusoft;
+
+public class  StudentJavaBean{
+    // 成员变量
+    private String name;
+    private int age;
+
+    // 无参构造方法【必须】
+    public StudentJavaBean(){
+    }
+    // 有参构造方法【建议】
+    public StudentJavaBean(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    // 成员方法
+    // getxxx()
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    // setxxx()
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
