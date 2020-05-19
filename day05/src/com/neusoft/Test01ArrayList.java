f85d8a1845dc406cf28ff038c9b87974245b39fe
NC0116 <63992297+NC0116@users.noreply.github.com>
2020-05-19 14:51:04 +0800

    ArrayList类----->大小可变的数组

---
+++ day05/src/com/neusoft/Test01ArrayList.java
@@ -0,0 +1,37 @@
+package com.neusoft;
+import  java.util.ArrayList;
+import java.util.Random;
+public class Test01ArrayList {
+    public static void main(String[] args) {
+        // 1.生成 10个 1到 33之间的随机整数，添加到集合中, 并且遍历输出
+
+        Random random = new Random();
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            int r = random.nextInt(33) + 1;
+            list.add(r);
+        }
+//        for (int i = 0; i <list.size() ; i++) {
+//            System.out.println(list.get(i));
+//        }
+        printArrayList(list);
+    }
+
+    // 自定义方法打印 list
+    public static void printArrayList(ArrayList<Integer> list) {
+        // 拼接左括号
+        System.out.print("[");
+        // 遍历集合
+        for (int i = 0; i < list.size(); i++) {
+            // 获取元素
+            Integer num = list.get(i);
+            if (i != list.size() - 1) {
+                System.out.print(num + ", ");
+            } else {
+                // 拼接右括号
+                System.out.println(num + "]");
+            }
+        }
+    }
+}
