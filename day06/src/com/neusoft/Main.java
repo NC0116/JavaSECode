c05d86738d899dfb0a52d13651db84e3b8c89264
NC0116 <63992297+NC0116@users.noreply.github.com>
2020-05-20 13:18:42 +0800

    String 类

---
+++ day06/src/com/neusoft/Main.java
@@ -0,0 +1,30 @@
+package com.neusoft;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        //String 类 JAVA.Lang包下
+        String s1= "abc";
+        System.out.println(s1);
+        String s2 = "abc";
+        System.out.println(s2);
+       // s1 += "d";
+        System.out.println(s1);
+        // 上面abc的创建相当于 new string 传入char型数组
+        char data[] = {'a','b', 'c'};
+        String string = new String(data); // String底层是 字符数组实现的
+        System.out.println(string);
+        System.out.println(string==s1); //false
+        System.out.println(s1.equals(s2)); //true
+        System.out.println(s1.equals(string)); //true
+
+//        String abc = new String("abc");
+//        System.out.println(abc);
+
+
+
+
+    }
+}
