8a0c6c581368a8a6283cb36a5e2558fa84fc1ab5
NC0116 <63992297+NC0116@users.noreply.github.com>
2020-06-10 19:34:58 +0800

    Revert "类实现接口"
    This reverts commit 3ea346a2946c0e995acdbc3825724997ee0bc404.

--- day07/src/com/neusoft/InterfaceDemoTest.java
+++
@@ -1,9 +0,0 @@
-package com.neusoft;
-
-public class InterfaceDemoTest {
-    public static void main(String[] args) {
-        AnimalLivable animalLivable = new AnimalLivable();
-        animalLivable.eat();
-        animalLivable.sleep();
-    }
-}
--- day07/src/com/neusoft/Livable.java
+++
@@ -1,7 +0,0 @@
-package com.neusoft;
-
-public interface Livable {
-    //定义抽象方法
-    public abstract void eat();
-    public void sleep();
-}
