b57a66f7216deae900bbf9a32cc6a57515b0e703
NC0116 <63992297+NC0116@users.noreply.github.com>
2020-06-17 15:55:44 +0800

    接口

---
+++ day09/src/com/neusoft/DoubleNumTest.java
@@ -0,0 +1,36 @@
+package com.neusoft;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DoubleNumTest {
+    public static void main(String[] args) {
+
+        //创建一个样本数据
+        List<Integer> srcList = new ArrayList();
+        for (int i=0; i<10; i++){
+            srcList.add(i);
+        }
+
+        //调用获取所有偶数的方法
+        List<Integer> list = getEvenNum(srcList);
+        System.out.println(list);
+    }
+
+    //获取某集合中所有的偶数
+    //List<Integer>  list作为了getEvenNum（）的方法参数
+    public static List<Integer> getEvenNum(List<Integer> list){
+        List<Integer> arrayList = new ArrayList();
+        //遍历集合
+        for (int i = 0; i < list.size(); i++) {
+            Integer integer = list.get(i);
+            if (integer%2 == 0)
+                arrayList.add(integer);
+        }
+        //因为getEvenNum方法返回值类型是List
+        //arrayList 是List的子类， 所以arrayList是可以返回的
+        //接口对应的类型作为返回值类型
+        return arrayList;
+    }
+}
